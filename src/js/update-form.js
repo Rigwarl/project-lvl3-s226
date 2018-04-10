628002353f5567b3fc0bafb2abe6e62840472951
Rigwarl <Avips@mail.ru>
2018-04-10 21:36:02 +0300

    improve form states

--- src/js/update-form.js
+++ src/js/update-form.js
@@ -1,11 +1,13 @@
-const updateForm = ($form, data) => {
-  const $urlInput = $form.find('[name=rss-url]');
+const updateForm = ($form, { valid, value, error }) => {
+  const $urlInput = $form.find('[name=url]');
+  const $submitBtn = $form.find('[name=submit]');
   const $urlError = $urlInput.next('.invalid-feedback');
-  const method = data.valid ? 'removeClass' : 'addClass';
 
-  $urlInput.val(data.value);
-  $urlInput[method]('is-invalid');
-  $urlError.text(data.errorMessage);
+  $urlInput.addClass(valid ? 'is-valid' : 'is-invalid');
+  $urlInput.removeClass(valid ? 'is-invalid' : 'is-valid');
+  $urlInput.val(value);
+  $urlError.text(error);
+  $submitBtn.prop('disabled', !valid);
 };
 
 export default updateForm;
