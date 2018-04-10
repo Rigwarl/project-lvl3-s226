8b5d952fcef4882c34c3e43d9fde9967fa29eedb
Rigwarl <Avips@mail.ru>
2018-04-10 13:06:59 +0300

    load rss feed

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,24 @@
 import $ from 'jquery';
+import axios from 'axios';
 import checkUrl from './url-checker';
 import createFormController from './form-controller';
 
 const init = () => {
+  const proxy = 'https://crossorigin.me';
   const rssMap = new Map();
 
-  const onSubmit = (url) => {
+  const form = $('#rss-form');
+
+  const addRss = (url) => {
     rssMap.set(url, []);
+    axios.get(`${proxy}/${url}`)
+      .then(res => console.log(res));
   };
 
   createFormController({
-    form: $('#rss-form'),
+    form,
+    onSubmit: addRss,
     checkUrl: url => checkUrl(url, rssMap),
-    onSubmit,
   });
 };
 
