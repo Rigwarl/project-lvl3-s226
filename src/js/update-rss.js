89d38b27486cccc041c52b8b6fd6ea30975fb51a
Rigwarl <Avips@mail.ru>
2018-04-13 23:51:26 +0300

    save active tab after update

--- src/js/update-rss.js
+++ src/js/update-rss.js
@@ -11,9 +11,9 @@ const createItem = (feedId, { id, url, title }) =>
   </li>`;
 
 const updateRss = ($element, rssArr) => {
-  const tabs = rssArr.map(({ id, title, description }, i) =>
+  const tabs = rssArr.map(({ id, title, description }) =>
     `<a 
-      class="list-group-item list-group-item-action ${i === 0 ? 'active' : ''}"
+      class="list-group-item list-group-item-action"
       href="#rss-feed-${id}"
       data-toggle="list"
     >
@@ -21,19 +21,24 @@ const updateRss = ($element, rssArr) => {
       <div>${description}</div>
     </a>`).join('');
 
-  const tabPanes = rssArr.map(({ id, items }, i) =>
-    `<ul class="tab-pane ${i === 0 ? 'active' : ''}" id="rss-feed-${id}">
+  const tabPanes = rssArr.map(({ id, items }) =>
+    `<ul class="tab-pane" id="rss-feed-${id}">
       ${items.map(item => createItem(id, item)).join('')}
     </ul>`).join('');
 
+  const activeItemHref = $element.find('.active').first().attr('href') || '#rss-feed-0';
+
   $element.html(`
     <div class="col-4">
-      <div class="list-group" role="tab">${tabs}</div>
+      <div class="list-group">${tabs}</div>
     </div>
     <div class="col-8">
       <div class="tab-content">${tabPanes}</div>
     </div>
   `);
+
+  $element.find(activeItemHref).addClass('active');
+  $element.find(`[href="${activeItemHref}"]`).addClass('active');
 };
 
 export default updateRss;
