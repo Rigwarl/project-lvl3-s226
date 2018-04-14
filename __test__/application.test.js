da30240b6cba1266aceb7627ec539ba079d29541
Rigwarl <Avips@mail.ru>
2018-04-14 20:48:19 +0300

    add form tests

---
+++ __test__/application.test.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import { html } from 'js-beautify';
+
+import run from '../src/js/application';
+
+const initHtml = fs.readFileSync(path.join(__dirname, '../src/template.html')).toString();
+const inputEvent = new Event('input', { bubbles: true });
+const getTree = () => html(document.body.innerHTML);
+
+beforeAll(() => {
+  document.documentElement.innerHTML = initHtml;
+  run();
+});
+
+test('form', () => {
+  const form = document.querySelector('#rss-form');
+  const urlInput = form.querySelector('[data-selector="url-input"]');
+  const submitBtn = form.querySelector('[data-selector="submit"]');
+
+  // ready
+  expect(getTree()).toMatchSnapshot();
+
+  // invalid
+  urlInput.value = 'invalid url';
+  urlInput.dispatchEvent(inputEvent);
+  expect(getTree()).toMatchSnapshot();
+
+  // valid
+  urlInput.value = 'valid.url';
+  urlInput.dispatchEvent(inputEvent);
+  expect(getTree()).toMatchSnapshot();
+
+  // empty
+  urlInput.value = '';
+  urlInput.dispatchEvent(inputEvent);
+  expect(getTree()).toMatchSnapshot();
+
+  // disabled
+  urlInput.value = 'valid.url';
+  urlInput.dispatchEvent(inputEvent);
+  submitBtn.click();
+  expect(getTree()).toMatchSnapshot();
+});
