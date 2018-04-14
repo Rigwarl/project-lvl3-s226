35e12ae625fdbf943cf73d0a005398b5f74c92aa
Rigwarl <Avips@mail.ru>
2018-04-14 21:16:35 +0300

    add disabled test

--- __test__/application.test.js
+++ __test__/application.test.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { html } from 'js-beautify';
+import axios from 'axios';
 
 import run from '../src/js/application';
 
@@ -12,6 +13,8 @@ beforeAll(() => {
   document.documentElement.innerHTML = initHtml;
   run();
 });
+
+jest.mock('axios');
 
 test('form', () => {
   const form = document.querySelector('#rss-form');
@@ -26,17 +29,18 @@ test('form', () => {
   urlInput.dispatchEvent(inputEvent);
   expect(getTree()).toMatchSnapshot();
 
-  // valid
-  urlInput.value = 'valid.url';
-  urlInput.dispatchEvent(inputEvent);
-  expect(getTree()).toMatchSnapshot();
-
   // empty
   urlInput.value = '';
   urlInput.dispatchEvent(inputEvent);
   expect(getTree()).toMatchSnapshot();
 
+  // valid
+  urlInput.value = 'valid.url';
+  urlInput.dispatchEvent(inputEvent);
+  expect(getTree()).toMatchSnapshot();
+
   // disabled
+  axios.get.mockResolvedValue({ data: '' });
   urlInput.value = 'valid.url';
   urlInput.dispatchEvent(inputEvent);
   submitBtn.click();
