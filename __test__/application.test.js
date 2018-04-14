55f289d1f0c80f4a728d389fbd5fc48eeed1ca1f
Rigwarl <Avips@mail.ru>
2018-04-14 21:50:02 +0300

    add form checks

--- __test__/application.test.js
+++ __test__/application.test.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import { html } from 'js-beautify';
+import timer from 'timer-promise';
 import axios from 'axios';
 
 import run from '../src/js/application';
 
 const initHtml = fs.readFileSync(path.join(__dirname, '../src/template.html')).toString();
+const hexletFeed = fs.readFileSync(path.join(__dirname, './feeds/hexlet.xml')).toString();
 const inputEvent = new Event('input', { bubbles: true });
 const getTree = () => html(document.body.innerHTML);
 
@@ -16,7 +18,7 @@ beforeAll(() => {
 
 jest.mock('axios');
 
-test('form', () => {
+test('form', async () => {
   const form = document.querySelector('#rss-form');
   const urlInput = form.querySelector('[data-selector="url-input"]');
   const submitBtn = form.querySelector('[data-selector="submit"]');
@@ -40,9 +42,28 @@ test('form', () => {
   expect(getTree()).toMatchSnapshot();
 
   // disabled
-  axios.get.mockResolvedValue({ data: '' });
+  axios.get.mockRejectedValue();
+  urlInput.value = 'valid.url';
+  urlInput.dispatchEvent(inputEvent);
+  submitBtn.click();
+  expect(getTree()).toMatchSnapshot();
+
+  // error
+  await timer.start(100);
+  expect(getTree()).toMatchSnapshot();
+
+  // loaded feed
+  axios.get.mockResolvedValue({ data: hexletFeed });
+  urlInput.value = 'valid.url';
+  urlInput.dispatchEvent(inputEvent);
+  submitBtn.click();
+  await timer.start(100);
+  expect(getTree()).toMatchSnapshot();
+
+  // exists
   urlInput.value = 'valid.url';
   urlInput.dispatchEvent(inputEvent);
   submitBtn.click();
   expect(getTree()).toMatchSnapshot();
 });
+
